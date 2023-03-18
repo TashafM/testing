46ae160b322cfa02cfb65ec10d146f7bdbf48a8a
TashafM <thecomputerpitch@gmail.com>
2023-03-18 16:42:21 +0530

    changes

---
+++ src/pages/Home/Dummy/dummydata.js
@@ -0,0 +1,12 @@
+export const dummyData = [
+  { title: "First Name", value: "firstName" },
+  // { title: "Last Name", value: "lastName" },
+  { title: "Personal Email", value: "personalEmail" },
+  { title: "Display Email", value: "displayEmail" },
+  { title: "Contact No.", value: "phone" },
+  { title: "Display Phone", value: "displayPhone" },
+  { title: "Department", value: "department" },
+  { title: "Designation", value: "designation" },
+  { title: "Employee Code", value: "emp_code" },
+  // { title: "Location", value: "location" },
+];
