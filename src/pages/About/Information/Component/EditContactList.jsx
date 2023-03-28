1a96089dac64b4e164b2de81962cc049cfa2cd99
umesh <umeshkukreti@tractorjunction.com>
2023-03-28 23:07:22 +0530

    contact us added

--- src/pages/About/Information/Component/EditContactList.jsx
+++ src/pages/About/Information/Component/EditContactList.jsx
@@ -1,18 +1,47 @@
 import React, { Fragment, useEffect, useState } from "react";
 import "./styles.scss";
 import { useContextProvider } from "../../../../context";
-import CardContacts from "./CardContacts";
+import email from "../../../../assets/images/email.png";
+import phone from "../../../../assets/images/phone.png";
 import chat from "../../../../assets/images/chat-icon.png";
 import Switch from "../../../../components/Input/Switch";
+import BtnIconOnly from "../../../../components/Button/BtnIconOnly";
+import edit from "../../../../assets/images/edit-icon.png";
+import delet from "../../../../assets/images/delete.svg";
+import { usePostAsyncResponse } from "../../../../hooks/usePostAsyncResponse";
+import BtnTitleCenter from "../../../../components/Button/BtnTitleCenter";
 
 function EditContactList() {
   const { openDrawer, setOpenDrawer } = useContextProvider();
   const [data, setData] = useState(openDrawer.data ?? []);
 
-  console.log({ openDrawer });
+  const [deleteData, { loading }] = usePostAsyncResponse(
+    "/portalDeleteCompanyContactUsDetails"
+  );
+
   useEffect(() => {
     setData(openDrawer.data ?? []);
   }, [openDrawer]);
+
+  const onDeletecontacts = (item, index) => {
+    const body = {
+      contactUsId: item._id,
+    };
+
+    deleteData(body, (res) => {
+      console.log({ openDrawer });
+      const arr = JSON.parse(JSON.stringify(data));
+      arr.splice(index, 1);
+
+      const d = JSON.parse(JSON.stringify(openDrawer.completeData));
+      d[0].contactUs = arr;
+
+      console.log({ d });
+      openDrawer.callback(d);
+
+      setData(arr);
+    });
+  };
 
   const onEnableChat = (index) => {
     const arr = [...data];
@@ -22,22 +51,56 @@ function EditContactList() {
 
   return (
     <Fragment>
+      <div className="btn-add-container">
+        <BtnTitleCenter
+          title="Add"
+          onClick={() => {
+            setOpenDrawer({
+              ...openDrawer,
+              type: "Add Contact",
+              data: {},
+            });
+          }}
+        />
+      </div>
+
       {data.map((item, index) => {
         return (
-          <CardContacts
-            key={item._id}
-            type={"Edit Contact"}
-            title={item?.title ?? ""}
-            contactUs={{ ...item }}
-            onClick={() => {
-              setOpenDrawer({
-                ...openDrawer,
-                type: "Edit Contact",
-                data: { ...item },
-                index: index,
-              });
-            }}
-          >
+          <div className="contact-us-container">
+            <div
+              className={`d-flex justify-content-center align-items-center `}
+            >
+              <div className=" wrapper-title">
+                <span className="cont-title contact-us-title">
+                  {item.title}
+                </span>
+                <div className="d-flex">
+                  <BtnIconOnly
+                    icon={delet}
+                    onClick={() => onDeletecontacts(item, index)}
+                  />
+                  <BtnIconOnly
+                    icon={edit}
+                    onClick={() => {
+                      setOpenDrawer({
+                        ...openDrawer,
+                        type: "Edit Contact",
+                        data: { ...item },
+                        index: index,
+                      });
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="d-flex email-cont">
+              <img className="" height={25} src={email} alt="email-icon" />
+              <span className="email">{item?.email ?? ""}</span>
+            </div>
+            <div className="d-flex">
+              <img height={25} src={phone} alt="phone-icon" />
+              <span className="email phone">{item?.contact ?? ""}</span>
+            </div>
             <div className="d-flex justify-content-between chat-container">
               <div className="d-flex">
                 <img className="card-img" src={chat} alt="chat-icon" />
@@ -50,7 +113,7 @@ function EditContactList() {
                 }}
               />
             </div>
-          </CardContacts>
+          </div>
         );
       })}
     </Fragment>
