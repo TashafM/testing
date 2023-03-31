241ef794c7701db4538943e6be061801511f70ab
TashafM <thecomputerpitch@gmail.com>
2023-03-31 10:21:05 +0530

    changes

--- src/pages/Dealers/Dealers.jsx
+++ src/pages/Dealers/Dealers.jsx
@@ -1,50 +1,50 @@
-import React from 'react'
-import { Outlet } from 'react-router'
-import Menus from '../../components/Menus/Menus'
-import MobileNotSupported from '../../components/MobileNotSupported/MobileNotSupported'
-import NavbarTop from '../../components/NavbarTop/NavbarTop'
-import NeedHelp from '../../components/NeedHelp/NeedHelp'
-import TabletNavbar from '../../components/TabletNavbar/TabletNavbar'
-import TabMenus from '../../components/TabMenus/TabMenus'
-import { dealerMenu } from './data'
+import React from "react";
+import { Outlet } from "react-router";
+import Menus from "../../components/Menus/Menus";
+import MobileNotSupported from "../../components/MobileNotSupported/MobileNotSupported";
+import NavbarTop from "../../components/NavbarTop/NavbarTop";
+import NeedHelp from "../../components/NeedHelp/NeedHelp";
+import TabletNavbar from "../../components/TabletNavbar/TabletNavbar";
+import TabMenus from "../../components/TabMenus/TabMenus";
+import { dealerMenu } from "./data";
 
 const Dealers = () => {
   return (
     <>
-        <div className="homepage">
-          <div className="forDesktop">
-            <NavbarTop />
-          </div>
-          <div className="forTablets">
-            <TabletNavbar />
-          </div>
-          <div className="tablet-menu">
-            <TabMenus />
-          </div>
-          <div className="row">
-            <div className="col-xl-2 menu-list">
-              <div className="sub-menu-list">
-                <Menus tabs={dealerMenu}/>
-                {/* <Button onClick={()=>{
+      <div className="homepage">
+        <div className="forDesktop">
+          <NavbarTop />
+        </div>
+        <div className="forTablets">
+          <TabletNavbar />
+        </div>
+        <div className="tablet-menu">
+          <TabMenus />
+        </div>
+        <div className="row">
+          <div className="col-xl-2 menu-list">
+            <div className="sub-menu-list">
+              <Menus tabs={dealerMenu} />
+              {/* <Button onClick={()=>{
                   setA(true)
                   console.log(a,'aaaaaaaaaa')
                 }}> click here</Button> */}
-                <NeedHelp />
-              </div>
-            </div>
-            <div className="col-xl-8 col-lg-12 content-area">
-              {/* <div className="sub-content-area">
-            <Outlet/>
-          </div> */}
-              <Outlet name="alkjaljfsa" />
             </div>
           </div>
+          <div className="col-xl-8 col-lg-12 content-area">
+            {/* <div className="sub-content-area">
+            <Outlet/>
+          </div> */}
+            <Outlet name="alkjaljfsa" />
+          </div>
+          <div className="col-xl-2 noti-section"></div>
         </div>
+      </div>
       <div className="mobile-not-supported">
         <MobileNotSupported />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Dealers
+export default Dealers;
--- src/pages/Dealers/SubPages/Dashboard/DealerDashboard.jsx
+++ src/pages/Dealers/SubPages/Dashboard/DealerDashboard.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import progress from '../../../../assets/images/progress.png'
 
 const DealerDashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="dashboard-container">
+      <Row>
+        <Col className="column"></Col>
+        <Col className="column"></Col>
+        <Col className="column"></Col>
+        <Col className="column"></Col>
+      </Row>
+      <Row>
+        <Col className="bottom-column">
+          <div className="progress-banner">
+            <div>
+              <img src={progress} alt="" />
+            </div>
+            <div className="txt">Work in progress...</div>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
-export default DealerDashboard
+export default DealerDashboard;
