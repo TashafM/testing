7ebaa5010b9df6ba74d8f35e0904e8fbaa7b96bc
TashafM <thecomputerpitch@gmail.com>
2023-04-10 08:53:54 +0530

    dfafa

--- src/pages/Dealers/SubPages/AllProducts/Products/Products.jsx
+++ src/pages/Dealers/SubPages/AllProducts/Products/Products.jsx
@@ -6,17 +6,17 @@ import "./products.scss";
 import { superItems } from "../../../data";
 import SidePanel from "../../../Components/SidePanel/SidePanel";
 import { GlobalSidePanel } from "../../../Dealers";
-import wishList from '../../../../../assets/images/wishlist.svg'
+import wishList from "../../../../../assets/images/wishlist.svg";
 
 const Products = () => {
-  const {showPanel, setShowPanel} = useContext(GlobalSidePanel)
+  const { showPanel, setShowPanel } = useContext(GlobalSidePanel);
   const location = useLocation();
-  const [currentId, setCurrentId] = useState(location.state.id)
+  const [currentId, setCurrentId] = useState(location.state.id);
 
   return (
     <>
       <div className="products">
-        <TopBar title={"All Products"} goback={true}/>
+        <TopBar title={"All Products"} goback={true} />
 
         <Row className="products-row">
           {superItems.map((item) =>
@@ -27,14 +27,16 @@ const Products = () => {
                       <div
                         className="img-category-main"
                         // style={{ backgroundImage: `url(${itm.image})`, backgroundRepeat: 'no-repeat' }}
-                        onClick={()=>setShowPanel(true)}
+                        onClick={() => setShowPanel(true)}
                       >
-                        <div className="wish-list-icon"><img src={wishList} alt="" /></div>
-                        <div className="product-img"><img src={itm.image} alt="" /></div>
-                        <div className="title">{itm.pname}</div>
-                        <div className="description">
-                          {itm.desc}
+                        <div className="wish-list-icon">
+                          <img src={wishList} alt="" />
                         </div>
+                        <div className="product-img">
+                          <img src={itm.image} alt="" />
+                        </div>
+                        <div className="title">{itm.pname}</div>
+                        <div className="description">{itm.desc}</div>
                       </div>
                     </Col>
                   ))
@@ -42,17 +44,22 @@ const Products = () => {
             )
           )}
         </Row>
-        <SidePanel show={showPanel} setShowPanel={setShowPanel}/>
+        <SidePanel show={showPanel} setShowPanel={setShowPanel} />
       </div>
       <div className="bottom-strip">
         <div className="bottom-brands">
-          {superItems.map((item)=>(
-            item.subItems.map((subitem)=>(
-              <div className="brand-div" onClick={()=>setCurrentId(subitem.id)}>
+          {superItems.map((item) =>
+            item.subItems.map((subitem) => (
+              <div
+                className={
+                  currentId == subitem.id ? "brand-div active-div" : "brand-div"
+                }
+                onClick={() => setCurrentId(subitem.id)}
+              >
                 <img src={subitem.subImg} />
               </div>
             ))
-          ))}
+          )}
         </div>
       </div>
     </>
