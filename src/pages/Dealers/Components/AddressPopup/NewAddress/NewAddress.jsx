cabc60836d78699c77615e823e04e876c15f9965
TashafM <thecomputerpitch@gmail.com>
2023-04-19 23:48:35 +0530

    changes

--- src/pages/Dealers/Components/AddressPopup/NewAddress/NewAddress.jsx
+++ src/pages/Dealers/Components/AddressPopup/NewAddress/NewAddress.jsx
@@ -1,4 +1,67 @@
+// import React, { useState } from "react";
+// import { newAddressField } from "../data";
+// import SimpleInput from "../../SimpleInput/SimpleInput";
+// import "./newaddress.scss";
+// import { Button } from "react-bootstrap";
+
+// const NewAddress = ({ setAddress, editMode }) => {
+//   const [addressType, setAddressType] = useState('shipping');
+
+//   const handleSetAddressType = (event) => {
+//     setAddressType(event.target.value);
+//     console.log(event.target.value);
+//   }
+
+//   return (
+//     <div className="new-address">
+//       {editMode ? null : (
+//         <>
+//           <div className="shipping-billing">This address is :</div>
+//           <div className="radio-div">
+//             <div className="shipping">
+//               <input
+//                 type="radio"
+//                 name="shipping-billing"
+//                 id="shipping"
+//                 value="shipping"
+//                 checked={addressType === "shipping"}
+//                 onChange={handleSetAddressType}
+//               />
+//               <label htmlFor="shipping">Shipping address</label>
+//             </div>
+//             <div className="billing">
+//               <input
+//                 type="radio"
+//                 name="shipping-billing"
+//                 id="billing"
+//                 value="billing"
+//                 checked={addressType === "billing"}
+//                 onChange={handleSetAddressType}
+//               />
+//               <label htmlFor="billing">Billing address</label>
+//             </div>
+//           </div>
+//         </>
+//       )}
+
+//       {newAddressField.map((item) => (
+//         <SimpleInput title={item.title} placeholder={item.placeholderText} />
+//       ))}
+
+//       <div className="button-div">
+//         <Button className="save-btn" onClick={() => setAddress(true)}>
+//           Save
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default NewAddress;
+
+
 import React from "react";
+import { Formik, Form, Field } from "formik";
 import { newAddressField } from "../data";
 import SimpleInput from "../../SimpleInput/SimpleInput";
 import "./newaddress.scss";
@@ -6,32 +69,66 @@ import { Button } from "react-bootstrap";
 
 const NewAddress = ({ setAddress, editMode }) => {
   return (
-    <div className="new-address">
-      {editMode ? null : (
-        <>
-          <div className="shipping-billing">This address is :</div>
-          <div className="radio-div">
-            <div className="shipping">
-              <input type="radio" name="address-type" id="" />
-              Shipping address
-            </div>
-            <div className="billing">
-              <input type="radio" name="address-type" id="" />
-              Billing address
-            </div>
-          </div>
-        </>
-      )}
-      {newAddressField.map((item) => (
-        <SimpleInput title={item.title} placeholder={item.placeholderText} />
-      ))}
+    <Formik
+      initialValues={{
+        addressType: "shipping",
+        ...newAddressField.reduce(
+          (acc, curr) => ({ ...acc, [curr.name]: "" }),
+          {}
+        ),
+      }}
+      onSubmit={(values) => {
+        console.log(values);
+        setAddress(true);
+      }}
+    >
+      {({ handleChange, values }) => (
+        <Form className="new-address">
+          {editMode ? null : (
+            <>
+              <div className="shipping-billing">This address is :</div>
+              <div className="radio-div">
+                <div className="shipping">
+                  <Field
+                    type="radio"
+                    name="addressType"
+                    id="shipping"
+                    value="shipping"
+                  />
+                  <label htmlFor="shipping">Shipping address</label>
+                </div>
+                <div className="billing">
+                  <Field
+                    type="radio"
+                    name="addressType"
+                    id="billing"
+                    value="billing"
+                  />
+                  <label htmlFor="billing">Billing address</label>
+                </div>
+              </div>
+            </>
+          )}
 
-      <div className="button-div">
-        <Button className="save-btn" onClick={() => setAddress(true)}>
-          Save
-        </Button>
-      </div>
-    </div>
+          {newAddressField.map((item) => (
+            <SimpleInput
+              key={item.name}
+              title={item.title}
+              placeholder={item.placeholderText}
+              value={values[item.name]}
+              onChange={handleChange}
+              name={item.name} // make sure the name attribute is set
+            />
+          ))}
+
+          <div className="button-div">
+            <Button className="save-btn" type="submit">
+              Save
+            </Button>
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
--- src/pages/Dealers/Components/AddressPopup/data.js
+++ src/pages/Dealers/Components/AddressPopup/data.js
@@ -2,42 +2,52 @@ export const newAddressField = [
     {
         title: 'Full Name*',
         placeholderText: 'Enter your full name',
+        name: 'fullname'
     },
     {
         title: 'Contact Number',
         placeholderText: 'Enter your contact number',
+        name: 'contact'
     },
     {
         title: 'House No. / Building No. / Floor*',
         placeholderText: 'Enter your house / building / floor no.',
+        name: 'house'
     },
     {
         title: 'Block',
         placeholderText: 'Enter your block',
+        name: 'block'
     },
     {
         title: 'Street*',
         placeholderText: 'Enter your street',
+        name: 'street'
     },
     {
         title: 'City*',
         placeholderText: 'Enter your city',
+        name: 'city'
     },
     {
         title: 'State*',
         placeholderText: 'Enter your state',
+        name: 'state'
     },
     {
         title: 'Country*',
         placeholderText: 'Enter your country',
+        name: 'country'
     },
     {
         title: 'Zip Code*',
         placeholderText: 'Enter your zip code',
+        name: 'zipcode'
     },
     {
         title: 'Google Places Location',
         placeholderText: 'Enter google places location',
+        name: 'googleplaces'
     },
 
 ]
