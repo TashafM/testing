4990fce016368751aa39f0cd097f41ebebfb9f72
umesh <umeshkukreti@tractorjunction.com>
2023-05-10 22:34:16 +0530

    ui changes done

--- src/pages/Dealers/SubPages/Favorites/Favorites.jsx
+++ src/pages/Dealers/SubPages/Favorites/Favorites.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Favorites = () => {
   return (
     <div className="upper-content about-sectio d-flex align-items-center justify-content-center">
-      <div className="txt">Feature Comming Soon...</div>
+      <div className="txt">Feature Coming Soon...</div>
     </div>
   );
 };
--- src/pages/Home/Payment/PaymentInfo.jsx
+++ src/pages/Home/Payment/PaymentInfo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function PaymentInfo() {
   return (
     <div className="upper-content about-sectio d-flex align-items-center justify-content-center">
-      <div className="txt">Feature Comming Soon...</div>
+      <div className="txt">Feature Coming Soon...</div>
     </div>
   );
 }
